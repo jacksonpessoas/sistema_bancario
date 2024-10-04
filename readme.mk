5ad80be8771d83cc0f408ba876f07b3cf725e0b5
JacksonPessoaS <jacksonpessoas@gmail.com>
2024-10-04 18:31:18 -0300

    v1 do sistema bancario

---
+++ readme.mk
@@ -0,0 +1,17 @@
+# Sistema de Gerenciamento de Conta Bancária
+
+Este é um simples sistema de gerenciamento de conta bancária em Python. O programa permite que o usuário realize operações de depósito, saque e consulte o extrato da conta. É uma implementação básica que pode ser expandida com mais funcionalidades.
+
+## Funcionalidades
+
+- **Depositar**: Permite que o usuário deposite um valor em sua conta.
+- **Sacar**: Permite que o usuário saque um valor da conta, respeitando o saldo disponível, o limite diário de saques e o número máximo de saques permitidos por dia.
+- **Extrato**: Exibe o extrato das operações realizadas, incluindo depósitos e saques, além do saldo atual.
+- **Sair**: Encerra o programa.
+
+## Como Usar
+
+1. **Execute o programa**: Você pode executar o código em um ambiente Python.
+2. **Selecione uma operação**: O menu será exibido, e você deve inserir o número da operação desejada (1 para Depositar, 2 para Sacar, 3 para Extrato, 4 para Sair).
+3. **Insira os valores necessários**: Para as operações de depósito e saque, você será solicitado a inserir o valor desejado.
+4. **Consulte o extrato**: Você pode visualizar o extrato de suas movimentações a qualquer momento.
